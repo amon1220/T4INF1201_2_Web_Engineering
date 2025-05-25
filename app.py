7d022781f32707d80fcd6ba4f5aa37fa9162f5eb
Jakob <77412019+DJSkyRoad@users.noreply.github.com>
2025-05-25 11:45:45 +0200

    Example App

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+
+@app.route('/next')
+def next_page():
+    return "This is the next page!"
+
+
+if __name__ == '__main__':
+    app.run()
