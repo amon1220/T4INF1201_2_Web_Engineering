f84a086b7202f13be3615c6f06a9996caa2584d9
László <laszlo.engemann@gmx.net>
2025-06-06 15:11:41 +0200

    Add Notepad icon-compontent (button) and Notepad-compontent. Pressing the button does nothing yet.

--- Frontend/frontend/src/App.js
+++ Frontend/frontend/src/App.js
@@ -1,11 +1,17 @@
 import './App.css';
 import Taskbar from './components/taskbar';
 
+import React, { useState } from "react";
+import NotepadButton from "./components/NotepadButton";
+import Notepad from "./components/Notepad";
+
 function App() {
     return (
         <div className="desktop">
             {/* später hier Icons oder Fenster */}
             <Taskbar />
+            <NotepadButton />
+            <Notepad />
         </div>
     );
 }
