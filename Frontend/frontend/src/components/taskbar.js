ba72e4d85cca1486c0e97169753cd88ce7a036d7
Laszlo2025 <laszlo.engemann@gmx.net>
2025-06-11 15:04:42 +0200

    Dynamic time

--- Frontend/frontend/src/components/taskbar.js
+++ Frontend/frontend/src/components/taskbar.js
@@ -3,18 +3,26 @@ import '98.css';
 
 export default function Taskbar() {
 
+    function updateTime() {
+        const currentdate = new Date().toLocaleTimeString();
+        //var datetime = currentdate.getHours() + ":" + currentdate.getMinutes();
+        document.getElementById("currenttime").textContent = currentdate;
+    }
+    setInterval(updateTime, 1000);
+
     return (
+
         <div className="taskbar">
             <button className="start-button">
                 <img src="https://win98icons.alexmeub.com/icons/png/windows-0.png" alt="start" />
                 Start
             </button>
             <div className="spacer" />
-            
-            <div className="clock">
 
-                8:40 PM
-            </div> {/* später dynamisch */}
+            <div className="clock">
+                <div id="currenttime"></div>
+            </div>
+
         </div>
     );
 }
