35740943539333b2292c443089aa65bd6ac5b839
Jakob <77412019+DJSkyRoad@users.noreply.github.com>
2025-06-20 11:18:20 +0200

    Fixed Typo

--- Backend/app.py
+++ Backend/app.py
@@ -6,7 +6,7 @@ from Backend.src import api
 app = Flask(__name__)
 CORS(app)
 
-app.register_blueprint(api.api_bp, url_prefix='/api')
+app.register_blueprint(api.api, url_prefix='/api')
 
 
 @app.route('/api/status', methods=['GET'])
