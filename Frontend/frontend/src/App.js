01fb08abda545d031d5430072bca76ef7b078f9f
Jakob <77412019+DJSkyRoad@users.noreply.github.com>
2025-06-20 21:20:13 +0200

    Extracted Desktop from App.js

--- Frontend/frontend/src/App.js
+++ Frontend/frontend/src/App.js
@@ -1,71 +1,11 @@
+import React from 'react';
 import './App.css';
-import Taskbar from './components/taskbar';
-
-import React, {useState} from "react";
-import NotepadButton from "./components/NotepadButton";
-import Notepad from "./components/Notepad";
-import RecycleBinButton from "./components/RecycleBinButton";
-import RecycleBin from "./components/RecycleBin";
-import IExplorer from "./components/IExplorer";
-import IExplorerButton from "./components/InternetExplorerButton";
-import HackingTool3000Button from "./components/HackingTool3000Button";
-import HackingTool3000 from "./components/HackingTool3000";
-import WeatherApp from "./components/WeatherApp";
-import WeatherAppButton from "./components/WeatherAppButton"
+import Desktop from './components/Desktop';
 
 function App() {
-    const [notepadOpen, setNotepadOpen] = useState(false); // true = geöffnet
-    const [recycleBinOpen, setRecycleBinOpen] = useState(false);
-    const [hackingToolOpen, setHackingTool3000Open] = useState(false);
-    const [iExplorerOpen, setIExplorerOpen] = useState(false);
-    const [myComputerOpen, setMyComputerOpen] = useState(false);
-    const [weatherAppOpen, setWeatherAppOpen] = useState(false);
-    const [startWindowOpen, setStartWindowOpen] = useState(false);
-
     return (
-        <div className="desktop">
-            <Taskbar
-                openWindows={{
-                    notepad: {isOpen: notepadOpen, title: "Notepad"},
-                    recycleBin: {isOpen: recycleBinOpen, title: "Recycle Bin"},
-                    iExplorer: {isOpen: iExplorerOpen, title: "Internet Explorer"},
-                    hackingTool: {isOpen: hackingToolOpen, title: "Hacking Tool 3000"},
-                    weatherApp: {isOpen: weatherAppOpen, title: "Weather App"}
-                }}
-                onWindowClick={{
-                    notepad: () => setNotepadOpen(true),
-                    recycleBin: () => setRecycleBinOpen(true),
-                    iExplorer: () => setIExplorerOpen(true),
-                    hackingTool: () => setHackingTool3000Open(true),
-                    weatherApp: () => setWeatherAppOpen(true)
-                }}
-            />
-
-            <RecycleBinButton onOpen={() => setRecycleBinOpen(true)}/>
-            {recycleBinOpen && (
-                <RecycleBin onClose={() => setRecycleBinOpen(false)}/>
-            )}
-
-
-            <NotepadButton onOpen={() => setNotepadOpen(true)}/>
-            {notepadOpen && (
-                <Notepad onClose={() => setNotepadOpen(false)}/>
-            )}
-            <IExplorerButton onOpen={() => setIExplorerOpen(true)}/>
-            {iExplorerOpen && (
-                <IExplorer onClose={() => setIExplorerOpen(false)}/>
-            )}
-            <HackingTool3000Button onOpen={() => setHackingTool3000Open(true)}/>
-            {hackingToolOpen && (
-                <HackingTool3000 onClose={() => setHackingTool3000Open(false)}/>
-            )}
-            <WeatherAppButton onOpen={() => setWeatherAppOpen(true)}/>
-            {weatherAppOpen && (
-                <WeatherApp onClose={() => setWeatherAppOpen(false)}/>
-            )}
-
-        </div>
-    );
+        <Desktop />
+    )
 }
 
 export default App;
---
+++ Frontend/frontend/src/components/Desktop.js
@@ -0,0 +1,71 @@
+import React, {useState} from "react";
+import "../App.css"
+import Taskbar from "./taskbar";
+import RecycleBinButton from "./RecycleBinButton";
+import RecycleBin from "./RecycleBin";
+import NotepadButton from "./NotepadButton";
+import Notepad from "./Notepad";
+import IExplorerButton from "./InternetExplorerButton";
+import IExplorer from "./IExplorer";
+import HackingTool3000Button from "./HackingTool3000Button";
+import HackingTool3000 from "./HackingTool3000";
+import WeatherAppButton from "./WeatherAppButton";
+import WeatherApp from "./WeatherApp";
+
+function Desktop() {
+
+    const [notepadOpen, setNotepadOpen] = useState(false); // true = geöffnet
+    const [recycleBinOpen, setRecycleBinOpen] = useState(false);
+    const [hackingToolOpen, setHackingTool3000Open] = useState(false);
+    const [iExplorerOpen, setIExplorerOpen] = useState(false);
+    const [myComputerOpen, setMyComputerOpen] = useState(false);
+    const [weatherAppOpen, setWeatherAppOpen] = useState(false);
+    const [startWindowOpen, setStartWindowOpen] = useState(false);
+
+    return (
+        <div className="desktop">
+            <Taskbar
+                openWindows={{
+                    notepad: {isOpen: notepadOpen, title: "Notepad"},
+                    recycleBin: {isOpen: recycleBinOpen, title: "Recycle Bin"},
+                    iExplorer: {isOpen: iExplorerOpen, title: "Internet Explorer"},
+                    hackingTool: {isOpen: hackingToolOpen, title: "Hacking Tool 3000"},
+                    weatherApp: {isOpen: weatherAppOpen, title: "Weather App"}
+                }}
+                onWindowClick={{
+                    notepad: () => setNotepadOpen(true),
+                    recycleBin: () => setRecycleBinOpen(true),
+                    iExplorer: () => setIExplorerOpen(true),
+                    hackingTool: () => setHackingTool3000Open(true),
+                    weatherApp: () => setWeatherAppOpen(true)
+                }}
+            />
+
+            <RecycleBinButton onOpen={() => setRecycleBinOpen(true)}/>
+            {recycleBinOpen && (
+                <RecycleBin onClose={() => setRecycleBinOpen(false)}/>
+            )}
+
+
+            <NotepadButton onOpen={() => setNotepadOpen(true)}/>
+            {notepadOpen && (
+                <Notepad onClose={() => setNotepadOpen(false)}/>
+            )}
+            <IExplorerButton onOpen={() => setIExplorerOpen(true)}/>
+            {iExplorerOpen && (
+                <IExplorer onClose={() => setIExplorerOpen(false)}/>
+            )}
+            <HackingTool3000Button onOpen={() => setHackingTool3000Open(true)}/>
+            {hackingToolOpen && (
+                <HackingTool3000 onClose={() => setHackingTool3000Open(false)}/>
+            )}
+            <WeatherAppButton onOpen={() => setWeatherAppOpen(true)}/>
+            {weatherAppOpen && (
+                <WeatherApp onClose={() => setWeatherAppOpen(false)}/>
+            )}
+
+        </div>
+    );
+}
+
+export default Desktop;
